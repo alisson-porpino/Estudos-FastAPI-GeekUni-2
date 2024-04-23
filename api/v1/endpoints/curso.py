9c5cdcb5815595dc0934529393a2c57f4dcefb82
Alisson Mateus Porpino dos Santos <alissonporpino.aps@gmail.com>
2024-04-23 16:04:39 -0300

    criando DB sem postgresql

---
+++ api/v1/endpoints/curso.py
@@ -0,0 +1,50 @@
+from typing import List
+
+from fastapi import APIRouter
+from fastapi import status
+from fastapi import Depends
+from fastapi import HTTPException
+from fastapi import Response
+
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.future import select
+
+from models.curso_model import CursoModel
+from schemas.curso_schema import CursoSchema
+from core.deps import get_session
+
+
+
+router = APIRouter ()
+# POST curso
+@router.post('/', status_code=status.HTTP_201_CREATED, response_model=CursoSchema)
+async def post_curso(curso: CursoSchema, db: AsyncSession = Depends(get_session)):
+    novo_curso = CursoModel(titulo=curso.titulo, aulas=curso.aulas, horas=curso.horas)
+
+    db.add(novo_curso)
+    await db.commit()
+
+    return novo_curso
+
+# GET cursos
+@router.get('/', response_model=List[CursoSchema])
+async def get_cursos(db: AsyncSession, Depends(get_session)):
+    async with db as session:
+        query = select(CursoModel)
+        result = await session.execute(query)
+        cursos: List[CursoModel] = result.scalars().all()
+
+        return cursos
+
+# GET curso
+@router.get('/{curso_id}', response_model=CursoSchema, status_code=status.HTTP_200_OK)
+async def get_curso(curso_id: int, db: AsyncSession = Depends(get_session)):
+    async with db as session:
+        query = select(CursoModel).filter(CursoModel.id == curso_id)
+        result = await session.execute(query)
+        curso = result.scalar_one_or_none
+
+        if curso:
+            return curso
+        else:
+            raise HTTPException(detail='Curso não encontrado.', status_code=status.HTTP_404_NOT_FOUND)
--- core/config.py
+++ core/config.py
@@ -8,7 +8,8 @@ class Settings(BaseSettings):
     Configurações Gerais Usadas na Aplicação.
     """
     API_V1_STR: str = '/api/v1'
-    DB_URL: str = 'postgresql+asyncpg://postgres:1212@localhost:5432/srs'
+    DB_URL: str = 'sqlite+aiosqlite:///./teste.db/'
+    # DB_URL: str = 'postgresql+asyncpg://postgres:1234@localhost:5432/srs'
     DBBaseModel: DeclarativeMeta = declarative_base()
 
     class config:
