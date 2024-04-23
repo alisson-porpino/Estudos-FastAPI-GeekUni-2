927bcc09bd88db48432d7017b72a4357e5d565d6
Alisson Mateus Porpino dos Santos <alissonporpino.aps@gmail.com>
2024-04-23 09:29:02 -0300

    Configuração do DB

--- core/config.py
+++ core/config.py
@@ -1,6 +1,5 @@
-from typing import List
-
-from pydantic import BaseSettings, AnyHttpUrl
+from pydantic_settings import BaseSettings
+from sqlalchemy.ext.declarative import DeclarativeMeta
 from sqlalchemy.ext.declarative import declarative_base
 
 
@@ -9,5 +8,13 @@ class Settings(BaseSettings):
     Configurações Gerais Usadas na Aplicação.
     """
     API_V1_STR: str = '/api/v1'
-    DB_URL: str = 'postgresql+asyncpg://nomedobanco@localhost:5432/faculdade'
-    DBBaseModel = declarative_base()
+    DB_URL: str = 'postgresql+asyncpg://postgres:1212@localhost:5432/srs'
+    DBBaseModel: DeclarativeMeta = declarative_base()
+
+    class config:
+        case_sensitive = True
+
+
+
+
+settings = Settings()
